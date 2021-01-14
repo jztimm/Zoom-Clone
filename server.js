742743a7b8d7f2251e9df7e2219ebdb9aee33dfc
jztimm <jztimm@gmail.com>
2021-01-13 20:52:54 -0500

    ready to run express

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ const express = require('express');
 const app = express();
 const server = require('http').Server(app);
 
-
+app.get('/', (req, res) => {
+  res.status(200).write("Hello World")
+})
 
 
 
